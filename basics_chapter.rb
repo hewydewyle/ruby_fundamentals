0c10ac91eba8e6e982e96a9193ad9c9dc5810b41
hewydewyle <le.hieu@gmail.com>
2015-03-01 00:15:21 -0500

    keep going

--- basics_chapter.rb
+++ basics_chapter.rb
@@ -20,3 +20,25 @@ puts thousands
 puts hundreds
 puts tens
 puts ones
+
+movies = {:jaws => "2004", #jaws: 2014
+          :big => "1988", #big: 2003
+          :pulp_fiction => "1993", #pulp_fiction: 1993
+          :shrek => "2001", #shrek: 2001
+          :scream => "1999" #scream: 1999
+}
+
+puts movies[:jaws]
+puts movies[:big]
+puts movies[:pulp_fiction]
+puts movies[:shrek]
+puts movies[:scream]
+
+
+dates = [2001, 2002, 2099, 1938, 1983]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
---
+++ continued_basics.rb
@@ -0,0 +1,14 @@
+
+five = 5 * 4 * 3 * 2 * 1
+six = 6 * 5 * 4 * 3 * 2 * 1
+seven = 7 * 6 * 5 * 4 * 3 * 2 * 1
+eight = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts five
+puts six
+puts seven
+puts eight
+
+puts 34.4324 * 2.52
+puts 4257.52 * 6.3545
+puts 90963.54 * 34.53
