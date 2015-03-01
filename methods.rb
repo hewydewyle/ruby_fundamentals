91704a3ea9419d6fc6a2486063c347b378d3f406
hewydewyle <le.hieu@gmail.com>
2015-03-01 17:12:59 -0500

    methods, conditional

---
+++ methods.rb
@@ -0,0 +1,22 @@
+#Write a program that contains a method called greeting that 
+#takes a name as its parameter. It then prints a greeting message with the name included in it.
+
+def greetings(name)
+  puts "hi there " + name
+end
+
+puts greetings("hieu")
+
+#Write a program that includes a method called multiply
+# that takes two arguments and returns the product of the two numbers.
+
+
+def multiply(fnum, snum)
+  fnum * snum
+end
+
+puts multiply(30, 40)
+
+
+
+
