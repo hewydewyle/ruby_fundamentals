44262b14fdb8d299494ccb8133ff9a14cf33136c
hewydewyle <le.hieu@gmail.com>
2015-03-02 00:32:33 -0500

    flow control

--- conditional.rb
+++ conditional.rb
@@ -14,7 +14,46 @@ elsif a == 4
 else
 
   puts "a is neither 3, nor 4"
-  
+
 end
 
 
+a = 5
+
+answer = case a
+
+  when 5 
+  
+   "a is 5"
+
+  when 6
+
+   "a is 6"
+
+  else
+
+    "a is neither 5, nor 6"
+
+  end
+
+puts answer
+
+
+def caps(words)
+
+  if words.length > 10
+
+    puts words.upcase
+  
+  else
+
+    puts words
+
+  end
+
+end
+
+
+puts caps("fooooooooooooddddd")
+
+puts caps("hieule")
