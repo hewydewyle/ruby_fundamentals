6deac251b53611b91447e9951df6f0965f66cbb7
hewydewyle <le.hieu@gmail.com>
2015-03-03 22:16:03 -0500

    index

--- check.rb
+++ check.rb
@@ -38,3 +38,13 @@ def fibonacci(number)
 end
 
 puts fibonacci(6)
+
+top_five_games = ["mario brothers",
+                  "excite bike",
+                  "ring king",
+                  "castlevania",
+                  "double dragon"]
+
+top_five_games.each_with_index do | game, index |
+  puts "#{index + 1}. #{game}"
+end
---
+++ recursion.rb
@@ -0,0 +1,16 @@
+def count_to_zero(num)
+  if num <= 0
+
+    puts num
+
+  else
+
+    puts num
+
+    count_to_zero(num - 1)
+  end
+
+end
+
+count_to_zero(3)
+count_to_zero(11)
